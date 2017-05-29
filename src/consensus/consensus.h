f812a85c8fcf622cb5d6de245ce092c745dfa75e
Jeff Garzik <jeff@bloq.com>
2017-05-29 03:19:27 -0400

    [consensus] Forward-port BIP102, 2M hard fork block size increase
    TODO items:  activation time buffer, sigops/cost alignment

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -8,14 +8,53 @@
 
 #include <stdint.h>
 
+/** BIP102 block size increase height */
+static const unsigned int BIP102_FORK_MIN_HEIGHT = 485218;
+
+// TODO
+// static const unsigned int BIP102_FORK_BUFFER = (90 * 144); // approx 3 mos.
+
 /** The maximum allowed size for a serialized block, in bytes (only for buffer size limits) */
 static const unsigned int MAX_BLOCK_SERIALIZED_SIZE = 4000000;
 /** The maximum allowed weight for a block, see BIP 141 (network rule) */
 static const unsigned int MAX_BLOCK_WEIGHT = 4000000;
 /** The maximum allowed size for a block excluding witness data, in bytes (network rule) */
-static const unsigned int MAX_BLOCK_BASE_SIZE = 1000000;
+static const unsigned int MAX_LEGACY_BLOCK_SIZE = (1 * 1000 * 1000);
+inline unsigned int MaxBlockBaseSize(int nHeight, bool fSegWitActive)
+{
+    static const unsigned int newSize = (2 * 1000 * 1000);
+
+    if (nHeight < (int)BIP102_FORK_MIN_HEIGHT)
+        return MAX_LEGACY_BLOCK_SIZE;
+
+    if (!fSegWitActive)
+        return MAX_LEGACY_BLOCK_SIZE;
+
+    // TODO: BIP102_FORK_BUFFER
+
+    return newSize;
+}
+
+inline unsigned int MaxBlockBaseSize()
+{
+    return MaxBlockBaseSize(99999999, true);
+}
+
+
 /** The maximum allowed number of signature check operations in a block (network rule) */
-static const int64_t MAX_BLOCK_SIGOPS_COST = 80000;
+static const int64_t MAX_LEGACY_SIGOPS_COST = 80000;
+inline int64_t MaxBlockSigOpsCost(int nHeight, bool fSegWitActive)
+{
+    return ((MaxBlockBaseSize(nHeight, fSegWitActive) / 50) * 4);
+}
+
+inline int64_t MaxBlockSigOpsCost()
+{
+    return MaxBlockSigOpsCost(99999999, true);
+}
+
+/** The maximum allowed number of transactions per block */
+static const unsigned int MAX_BLOCK_VTX_SIZE = 1000000;
 
 /** The maximum allowed size for a transaction, excluding witness data, in bytes */
 static const unsigned int MAX_TX_BASE_SIZE = 1000000;
